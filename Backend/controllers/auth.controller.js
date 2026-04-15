c2379ca57988c6b2dadd006f25a03b05296322dc
JITHIN LAKSHMAN <pjithinnavodaya@gmail.com>
2026-04-15 23:16:39 +0530

    Logout Functionality Done

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -89,5 +89,6 @@ export const login = async (req, res) => {
 
 //Logout
 export const logout = async (req, res) => {
-  res.send("Signup route");
+  res.clearCookie("token");
+  res.status(200).json({success:true,message:"Logged out successfully"})
 };
