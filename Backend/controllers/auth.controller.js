45eec7802df9653da768e2141fadfdf9509ccf69
JITHIN LAKSHMAN <pjithinnavodaya@gmail.com>
2026-04-16 07:23:03 +0530

    Backend Done

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -84,7 +84,21 @@ export const verifyEmail = async (req,res)=>{
 
 //Login 
 export const login = async (req, res) => {
-  res.send("Signup route");
+   const {email,password}=req.body;
+   try{
+    const user = await User.findOne({email});
+    if(!user){
+     return res.status(400).json({success:false,message:"Invalid credentials"});
+    }
+    const isPasswordValid = await bcryptjs.compare(password,user.password);
+    if(!isPasswordValid){
+       return res.status(400).json({success:false,message:"Invalid credentials"});
+    }
+    generatetokenAndSetCookie(res,user._id);
+
+    user.lastLogin =  new Date();
+    await user.save();
+   }
 };
 
 //Logout
