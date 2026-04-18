1e4b47b6220126b06ecbddd8a5a71f65bcc011c9
JITHIN LAKSHMAN <pjithinnavodaya@gmail.com>
2026-04-18 13:07:45 +0530

    Project Done

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,43 @@
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
-import authRoutes from "./routes/auth.route.js"
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+console.log("NODE_ENV =", process.env.NODE_ENV);
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-const app=express();
-const PORT=process.env.PORT || 5000
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5000",
+  process.env.CLIENT_URL
+];
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
-app.use("/api/auth",authRoutes);
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`server is running on port ${PORT} `)
-})
+app.use("/api/auth", authRoutes);
 
- 
+ if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server is running on port: ", PORT);
+});
